39e167c0c43bfb08846808ce79af61f7f76f63f9
Abou-sn <seckaboubacar1301@gmail.com>
2026-03-24 16:42:00 +0100

    Premier Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.applicalendrier {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.applicalendrier to javafx.fxml;
+    exports com.example.applicalendrier;
+}
