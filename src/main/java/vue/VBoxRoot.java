3c06a867c35b3f0e1bc5ad0ee52d3c6be713bcec
Aboubacar Seck <22501608@me.uvsq.fr>
2026-04-13 14:42:32 +0200

    TP2 1.1 Finish

--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -1,6 +1,7 @@
 package vue;
 
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -9,41 +10,46 @@ import modele.ConstantesCalendrier;
 import modele.DateCalendrier;
 import javafx.scene.control.Label;
 
+import java.util.List;
+
 public class VBoxRoot extends VBox {
 
     public VBoxRoot() {
         DateCalendrier today = new DateCalendrier();
-        int month = today.getMois();
-        int year = today.getAnnee();
-        CalendrierDuMois monthCalendar = new CalendrierDuMois(month, year);
 
-        Label titleLabel = new Label(ConstantesCalendrier.Mois.values()[month-1] + " " + year);
-        VBox.setMargin(titleLabel, new Insets(14));
-        this.getChildren().add(titleLabel);
-
-
-        StackPane monthStackPane = new StackPane();
-        VBox.setMargin(monthStackPane, new Insets(4));
-        this.getChildren().add(monthStackPane);
-
-        for (DateCalendrier date : monthCalendar.getDates()) {
+        StackPane monthsStackPane = new StackPane();
+        for (int month = 1; month <= 12; month++) {
+            CalendrierDuMois monthCalendar = new CalendrierDuMois(month,today.getAnnee());
+            ScrollPane monthScroll = new ScrollPane();
             VBox datesBox = new VBox();
 
-            Label dateLabel = new Label(date.toString());
-            VBox.setMargin(dateLabel, new Insets(8));
-            datesBox.getChildren().add(dateLabel);
-            ScrollPane scrollPane = new ScrollPane();
-            scrollPane.setContent(datesBox);
-            monthStackPane.getChildren().add(scrollPane);
+            monthScroll.setContent(datesBox);
+            monthScroll.setAccessibleText(ConstantesCalendrier.Mois.values()[month-1].toString());
 
-            // ajout des attributs id pour la feuille de style
-            if (date.getMois() != month) {
-                dateLabel.setId("dateOutOfMonth");
+            for (DateCalendrier date : monthCalendar.getDates()) {
+                Label dateLabel = new Label(date.toString());
+                datesBox.getChildren().add(dateLabel); // Ajouter à la Vbox courante locale, les dates du mois.
+                if (date.getMois() != month) {
+                    dateLabel.setId("dateOutOfMonth");
+                }
+                if (date.compareTo(today) == 0) {
+                    dateLabel.setId("today");
+                }
+
             }
-            if (date.compareTo(today) == 0) {
-                dateLabel.setId("today");
-            }
+            monthsStackPane.getChildren().add(monthScroll);
+
+
         }
+
+        List<Node> listScrolls = monthsStackPane.getChildren();
+        while (listScrolls.get(0).getAccessibleText().compareTo(ConstantesCalendrier.Mois.values()[today.getMois()].toString()) !=0) {
+            listScrolls.get(0).toFront();
+
+        }
+
+        this.getChildren().add(monthsStackPane);
+
     }
 }
 
