06149351f0a4c06ebbef84acce78a789820ad780
Aboubacar Seck <22501608@me.uvsq.fr>
2026-05-13 12:26:42 +0200

    Formualaire de reservation commencé

--- src/main/java/vue/GridPaneFormulaireReservation.java
+++ src/main/java/vue/GridPaneFormulaireReservation.java
@@ -1,9 +1,50 @@
 package vue;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class GridPaneFormulaireReservation extends GridPane {
+    private final int heureDebutDefault = 7;
+    private final int heureFinDefault = 18;
+    private final int minutesDefault = 0;
+
+    public void initialisation(){
+        setPadding(new Insets(55,10,0,50));
+        setHgap(15);
+        setVgap(15);
+        //Label du Cours et le champs d'entrée
+        add(new Label("Cours"),0,0);
+        add(new TextField(),1,0);
+
+        add(new Label("Horaire"),0,1); add(new Label("de"),1,1); add (new Label("à"),1,2);
+
+        ComboBox<Integer> heuresDebut = new ComboBox<>(); heuresDebut.setValue(heureDebutDefault);
+        ComboBox<Integer> minutesDebut = new ComboBox<>(); minutesDebut.setValue(minutesDefault);
+
+        ComboBox<Integer> heuresFin = new ComboBox<>(); heuresFin.setValue(heureFinDefault);
+        ComboBox<Integer> minutesFin = new ComboBox<>(); minutesFin.setValue(minutesDefault);
+
+        for(int i = heureDebutDefault; i< heureFinDefault +1; i++){
+            heuresDebut.getItems().add(i);
+            heuresFin.getItems().add(i);
+        } // Ajouter les heures dans la liste déroulante des heures
+
+        for (int i = 0; i < 4; i++) {
+            int quartDheure = i*15;
+            minutesDebut.getItems().add(quartDheure);
+            minutesFin.getItems().add(quartDheure);
+        }
+
+        add(heuresDebut,2,1); add(new Label("h"),3,1);add(minutesDebut,4,1); add(new Label("min"),5,1); //ajout de la liste des heures début
+        add(heuresFin,2,2); add(new Label("h"),3,2);add(minutesFin,4,2);add(new Label("min"),5,2); //Pareil pour heures de fin
+
+    }
+
     public GridPaneFormulaireReservation() {
-        this.setGridLinesVisible(true);
+        setGridLinesVisible(true);
+        initialisation();
     }
 }
